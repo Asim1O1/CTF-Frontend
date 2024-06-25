429291dad5a3b810493ec92268edd6a8dfbfba54
Aayush Thapa <aayushthapa@Aayushs-MacBook-Air.local>
2024-06-25 10:00:36 +0545

    nav bar and footer

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,31 +1,6 @@
-// import React from 'react';
-// // import Typed  from 'react-typed';
-
-// const Hero = () => {
-//   return (
-//     <div className='text-white'>
-//       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-//         <div className='flex justify-center items-center'>
-//           <h1 className='md:text-5xl sm:text-4xl text-xl text-black py-4 font-bold-Poppins'>
-//             NEPAL FIRST PLATFORM
-//           </h1>
-//           {/* <Typed
-//             className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-//             strings={['CTF', 'AAY', 'THA']}
-//             typeSpeed={120}
-//             backSpeed={140}
-//             loop
-//           /> */}
-//         </div>
-//         <button className='bg-red-600 text-white w-[200px] rounded-md font-medium my-6 mx-auto py-3 '>Learn More</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
 
 import React from 'react';
+import ParticlesBackground from './ParticlesBackground.jsx';
 
 const Hero = () => {
   return (
@@ -50,3 +25,29 @@ const Hero = () => {
 };
 
 export default Hero;
+// src/Components/Hero.jsx
+// import React from 'react';
+// import ParticlesBackground from './ParticlesBackground';
+
+// const Hero = () => {
+//   return (
+//     <div className="relative text-white h-screen bg-white">
+//       <ParticlesBackground />
+//       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+//         <div className="max-w-[800px] mt-[-96px] w-full h-full mx-auto text-center flex flex-col justify-center items-center">
+//           <div className="bg-white bg-opacity-10 p-10 justify-center rounded ml-50">
+//             <h1 className="md:text-5xl sm:text-4xl text-xl text-white py-4 font-bold-Poppins ml-50">
+//               NEPAL'S FIRST CTF PLATFORM
+//             </h1>
+//             <button className="bg-yellow-600 text-white w-[200px] rounded-md font-medium my-6 mx-auto py-3">
+//               Learn More
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Hero;
+
