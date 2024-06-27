e572b6621686c56f04339e002bf365b2c310ffa1
Aayushthapa1 <aayush134056@gmail.com>
2024-06-27 14:34:43 +0545

    all done

--- src/Components/UserDash.jsx
+++ src/Components/UserDash.jsx
@@ -7,11 +7,11 @@ const UserDash = () => {
         <h1 className="text-black text-Left pt-10 mx-[20px] font-[verdana] font-bold text-[29px] hover:text-[32px]">User Dashboard</h1>
         <p className='text-wblack font-regular mx-[20px]  '>A perfect place to showcase your skills.</p>
       </div>
-      <div className="bg-gray-200 opacity-50 h-[100px] rounded-md ">
-        <h2 className='text-black text-Left pt-10 mx-[20px] font-[verdana] font-regular text-[20px]  grid gap-[30px]'>
+      <div className="bg-gray-200 opacity-50 h-[70px] rounded-md ">
+        <h2 className='text-black text-Left pt-7 mx-[20px] font-[verdana] font-regular text-[20px]  grid gap-[30px]'>
             Recent Rooms           
         </h2>
-        <div className="h-[500px] w-[650px] bg-black">
+        <div className="h-[500px] w-[650px] bg-gray-100 shadow-2xl rounded-md">
 
         </div>
 
