15a92a8bf05a2ba8d901ee5fb153f5cbbff63c94
Asim1O1 <146157042+Asim1O1@users.noreply.github.com>
2024-06-28 16:08:27 +0545

    login logout state management completed

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const Button = () => {
   return (
-    <button className=' text-2xl h-[50px] w-[150px]  hover:bg-yellow-700 transition duration-300 ease-in-out  bg-yellow-600 text-white font-Poppins py-2 px-6 rounded md:ml-8 hover:bg-black-400 '></button>
+    <button className=' text-2xl h-[50px] w-[150px]  hover:bg-yellow-700 transition duration-300 ease-in-out  bg-yellow-600 text-white font-Poppins py-2 px-6 rounded md:ml-8 hover:bg-black-400 '><Link to={"/register"}>Register</Link></button>
   )
 }
 
